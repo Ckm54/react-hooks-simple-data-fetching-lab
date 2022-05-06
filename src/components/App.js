db465ceb380efce69da17eda9267992a52bd8179
Collins <mutaic84@gmail.com>
2022-05-06 10:34:21 +0300

    perform dog data fetch and display random dog image on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 // create your App component here
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      setImagesLoaded(data)
+      setIsLoading(true)
+    })
+  }, [])
+  
+  if (!isLoading) return <p>Loading...</p>
   return (
-    <div>images</div>
+    <img src={imagesLoaded.message} alt="A Random Dog" />
   )
 }
 
